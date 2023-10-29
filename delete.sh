1d8febe358a3a406191e3015c442f69476cd3fbc
aGuyOverThere <alienanalyzer@gmail.com>
2023-10-29 17:04:23 -0500

    deleted script

--- delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git checkout --orphan newBranch
-git add -A  # Add all files and commit them
-git commit
-git branch -D main  # Deletes the master branch
-git branch -m main  # Rename the current branch to master
-git push -f origin main  # Force push master branch to github
-git gc --aggressive --prune=all     # remove the old files
